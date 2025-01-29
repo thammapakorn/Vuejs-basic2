177d549f6e25528d0f32ac42fd5a95401edf3dc3
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-01-29 23:21:39 +0700

    สร้าง component person เพิ่ม

--- my-app-component/src/components/ListData.vue
+++ my-app-component/src/components/ListData.vue
@@ -1,16 +1,30 @@
 <template>
-    <h1>{{ message }}</h1>
+    <div>
+        <h1>{{ message }}</h1>
+        <!-- ถ้าอยากเรียกใช้หลายๆอันก็ เรียกใช้บ่อยๆนะ -->
+        <PersonComponent/>
+        <PersonComponent/>
+        <PersonComponent/>
+        <PersonComponent/>
+        
+    </div>
 </template>
 
 <script>
+import PersonComponent from './PersonComponent.vue';
+
 export default {
     name:"ListData",
+    components:{
+        PersonComponent
+    },
     data(){
         return{
-            message:"แสดงรายชื่อพนักงาน"
+            message:"แสดงรายชื่อพนักงานทุกคน"
         }
     }
-}
+  }
+
 </script>
 
 <style>
---
+++ my-app-component/src/components/PersonComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"PersonComponent",
+    data(){
+        return{
+            message:"ข้อมูลพนักงาน"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
