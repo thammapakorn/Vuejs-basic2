c6ffe06cbf893b43ad69ab20f98504df4889023e
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-01-29 22:39:10 +0700

    สร้าง component และนำมาใช้งานใน app.vue

--- my-app-component/src/App.vue
+++ my-app-component/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <h1>Root Component</h1>
-  </div>
+    <h1>App Component</h1>
+    <ListData/>
+</div>
 </template>
 
 <script>
+import ListData from './components/ListData.vue';
 export default{
-  name:"app"
+  name:"App",
+  components:{
+    ListData
+  }
 }
 </script>
 
---
+++ my-app-component/src/components/ListData.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+    name:"ListData",
+    data(){
+        return{
+            message:"ListData Component"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
